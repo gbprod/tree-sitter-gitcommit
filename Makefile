df30da121d6270c2fe9e5e2040defeb99576263f
Gilles Roustan <gilles.roustan@mediapart.fr>
2026-03-02 17:33:19 +0100

    chore: update all

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION := 0.0.1
+VERSION := 0.4.0
 
 LANGUAGE_NAME := tree-sitter-gitcommit
 
--- Package.swift
+++ Package.swift
@@ -1,47 +1,41 @@
 // swift-tools-version:5.3
+
+import Foundation
 import PackageDescription
+
+var sources = ["src/parser.c"]
+if FileManager.default.fileExists(atPath: "src/scanner.c") {
+    sources.append("src/scanner.c")
+}
 
 let package = Package(
     name: "TreeSitterGitcommit",
     products: [
         .library(name: "TreeSitterGitcommit", targets: ["TreeSitterGitcommit"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(name: "SwiftTreeSitter", url: "https://github.com/tree-sitter/swift-tree-sitter", from: "0.9.0"),
+    ],
     targets: [
-        .target(name: "TreeSitterGitcommit",
-                path: ".",
-                exclude: [
-                    "Cargo.toml",
-                    "Makefile",
-                    "binding.gyp",
-                    "bindings/c",
-                    "bindings/go",
-                    "bindings/node",
-                    "bindings/python",
-                    "bindings/rust",
-                    "prebuilds",
-                    "grammar.js",
-                    "package.json",
-                    "package-lock.json",
-                    "pyproject.toml",
-                    "setup.py",
-                    "test",
-                    "examples",
-                    ".editorconfig",
-                    ".github",
-                    ".gitignore",
-                    ".gitattributes",
-                    ".gitmodules",
-                ],
-                sources: [
-                    "src/parser.c",
-                    // NOTE: if your language has an external scanner, add it here.
-                ],
-                resources: [
-                    .copy("queries")
-                ],
-                publicHeadersPath: "bindings/swift",
-                cSettings: [.headerSearchPath("src")])
+        .target(
+            name: "TreeSitterGitcommit",
+            dependencies: [],
+            path: ".",
+            sources: sources,
+            resources: [
+                .copy("queries")
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")]
+        ),
+        .testTarget(
+            name: "TreeSitterGitcommitTests",
+            dependencies: [
+                "SwiftTreeSitter",
+                "TreeSitterGitcommit",
+            ],
+            path: "bindings/swift/TreeSitterGitcommitTests"
+        )
     ],
     cLanguageStandard: .c11
 )
